8e4f0d50d2d487af83135e07a24fbae2293e84e3
mariovarela99 <mariovarela99@outlook.com>
2021-02-18 17:31:21 +0100

    Dando um Console.log no Data

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React, { Component } from "react";
 import api from "./api";
 
 class App extends Component {
+  state = {
+    users: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get("");
+
+    this.setState({ users: response.data });
+  }
+
   render() {
-    return <h1>API Consumir Front React</h1>;
+    const { filmes } = this.state;
+    return (
+      <>
+        <h1>API Consumir Front React</h1> {console.log(filmes)}
+      </>
+    );
   }
 }
 
